228bfe974be1959f2eae36ba97419b87fb4a7fd0
Michal Gasiorowski <enkidupal@gmail.com>
2021-04-29 18:36:49 +0200

    test fixes

--- tfx_titanic_pipeline/pipeline/tests/pipeline_test.py
+++ tfx_titanic_pipeline/pipeline/tests/pipeline_test.py
@@ -9,14 +9,15 @@ import glob
 
 import tensorflow as tf
 
-from pipeline.local_runner import LocalRunner
+from pipeline.local_runner import LocalRunnerWrapper
+from pipeline.beam_dag_runner import BeamDagRunnerWrapper
 from pipeline.config import Config
 
 import pipeline.pipelines as pipelines
 
-
 from distutils.util import strtobool
 from tfx.orchestration import metadata
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 tests_path = os.getcwd()
@@ -53,52 +54,81 @@ class PipelineTest(tf.test.TestCase):
         os.environ["PYTHON_VERSION"] = '3.7'
         os.environ["ENABLE_TUNING"] = 'False'
         os.environ["ENABLE_CACHE"] = 'True'
-        os.environ["TRAIN_STEPS"] = '100'
-        os.environ["TUNER_STEPS"] = '50'
-        os.environ["EVAL_STEPS"] = '50'
-        os.environ["EPOCHS"] = '2'
+        os.environ["TRAIN_STEPS"] = '1000'
+        os.environ["TUNER_STEPS"] = '100'
+        os.environ["EVAL_STEPS"] = '100'
+        os.environ["EPOCHS"] = '5'
         os.environ["TRAIN_BATCH_SIZE"] = '64'
         os.environ["EVAL_BATCH_SIZE"] = '64'
         os.environ["MAX_TRIALS"] = '5'
 
     def setUp(self):
         self.env_config = Config()
-        self.local_runner = None
+        self.pipeline_root = None
 
     def tearDown(self):
-        shutil.rmtree(self.local_runner.LOCAL_PIPELINE_ROOT)
+        shutil.rmtree(self.pipeline_root)
         pass
 
     # test scenarios below
 
     def testLocalDagRunnerWithoutTuning(self):
-        self.local_runner = LocalRunner()
-        self.local_runner.create_pipeline_root_folders_paths()
+        runner = LocalRunnerWrapper()
+        runner.create_pipeline_root_folders_paths()
+        self.pipeline_root = runner.LOCAL_PIPELINE_ROOT
 
-        self.local_runner.run()
+        runner.run()
 
         for comp in PipelineTest.component_output_directories:
-            self.assertTrue(os.path.exists(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, comp)))
+            self.assertTrue(os.path.exists(os.path.join(runner.LOCAL_PIPELINE_ROOT, comp)))
 
-        self.assertNotEmpty(glob.glob(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/train/data_tfrecord*.*'), recursive=True))
-        self.assertNotEmpty(glob.glob(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/eval/data_tfrecord*.*'), recursive=True))
+        self.assertNotEmpty(
+            glob.glob(os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/train/data_tfrecord*.*'),
+                      recursive=True))
+        self.assertNotEmpty(
+            glob.glob(os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/eval/data_tfrecord*.*'),
+                      recursive=True))
 
     def testLocalDagRunnerWithTuning(self):
         os.environ["ENABLE_TUNING"] = 'True'
         os.environ["MAX_TRIALS"] = '5'
-        self.local_runner = LocalRunner()
-        self.local_runner.create_pipeline_root_folders_paths()
+        runner = LocalRunnerWrapper()
+        runner.create_pipeline_root_folders_paths()
+        self.pipeline_root = runner.LOCAL_PIPELINE_ROOT
 
-        self.local_runner.run()
+        runner.run()
 
         for comp in PipelineTest.component_output_directories_wth_tuning:
-            self.assertTrue(os.path.exists(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, comp)),
-                            msg=f'{comp} component directory doesnt exist in PIPELINE_ROOT ( {self.local_runner.LOCAL_PIPELINE_ROOT} ) ')
+            self.assertTrue(os.path.exists(os.path.join(runner.LOCAL_PIPELINE_ROOT, comp)),
+                            msg=f'{comp} component directory doesnt exist in PIPELINE_ROOT ( {runner.LOCAL_PIPELINE_ROOT} ) ')
 
         self.assertNotEmpty(
-            glob.glob(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/train/data_tfrecord*.*'), recursive=True))
+            glob.glob(os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/train/data_tfrecord*.*'),
+                      recursive=True))
         self.assertNotEmpty(
-            glob.glob(os.path.join(self.local_runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/eval/data_tfrecord*.*'), recursive=True))
+            glob.glob(os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/eval/data_tfrecord*.*'),
+                      recursive=True))
+
+    def testBeamDagRunnerWithoutTuning(self):
+        os.environ["ENABLE_TUNING"] = 'False'
+        runner = BeamDagRunnerWrapper()
+        runner.create_pipeline_root_folders_paths()
+        self.pipeline_root = runner.LOCAL_PIPELINE_ROOT
+
+        runner.run()
+
+        for comp in PipelineTest.component_output_directories:
+            self.assertTrue(os.path.exists(os.path.join(runner.LOCAL_PIPELINE_ROOT, comp)),
+                            msg=f'{comp} component directory doesnt exist in PIPELINE_ROOT ( {runner.LOCAL_PIPELINE_ROOT} ) ')
+
+        self.assertNotEmpty(
+            glob.glob(
+                os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/train/data_tfrecord*.*'),
+                recursive=True))
+        self.assertNotEmpty(
+            glob.glob(os.path.join(runner.LOCAL_PIPELINE_ROOT, 'CsvExampleGen/**/eval/data_tfrecord*.*'),
+                      recursive=True))
+
 
 if __name__ == '__main__':
     tf.test.main()
